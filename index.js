d35ca4933458b5a850f5fdd2c8ffe54771113445
scotch <liamcorbett01@gmail.com>
2021-08-18 14:03:47 +1000

    Wrap up menu, add integration with node program

--- index.js
+++ index.js
@@ -3,49 +3,68 @@ const fs = require('fs');
 const { re, complete } = require('./extract');
 const { Browser } = require('./note');
 
+process.argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+});
+
 async function main () {
-    const detailsFiles = fs.readdirSync('./data/details');
-    const eqidFiles = fs.readdirSync('./data/eqids');
-    if (!detailsFiles.length || !eqidFiles.length) {
-        console.error(`No files found in one of the data folders!\nExiting...`);
-    } else {
-        const fName = prompt('Enter staff first name: ');
-        const lName = prompt('Enter staff last name: ');
-        const user = prompt('Enter your MIS ID: ');
-        const pw = prompt('Enter your password: ', { echo: '*' });
-        let PIN = prompt('Enter your OneSchool PIN: ', { echo: '*' });
+    // const detailsFiles = fs.readdirSync('./data/details');
+    // const eqidFiles = fs.readdirSync('./data/eqids');
+    // if (!detailsFiles.length || !eqidFiles.length) {
+    //     console.error(`No files found in one of the data folders!\nExiting...`);
+    // } else {
+    //     const fName = prompt('Enter staff first name: ');
+    //     const lName = prompt('Enter staff last name: ');
+    //     const user = prompt('Enter your MIS ID: ');
+    //     const pw = prompt('Enter your password: ', { echo: '*' });
+    //     let PIN = prompt('Enter your OneSchool PIN: ', { echo: '*' });
 
-        PIN = PIN.split('');
+    //     PIN = PIN.split('');
     
-        const date = prompt('Enter the date of contact (dd-mm): ');
+    //     const date = prompt('Enter the date of contact (dd-mm): ');
 
-        const details = getDetailFile(detailsFiles).toString();
-        const eqidsToParse = [...getEQIDsFile(eqidFiles).toString().matchAll(re)];
+    //     const details = getDetailFile(detailsFiles).toString();
+    //     const eqidsToParse = [...getEQIDsFile(eqidFiles).toString().matchAll(re)];
         
-        // let eqids = eqidsToParse.map(v => v[0]);
+    //     // let eqids = eqidsToParse.map(v => v[0]);
 
-        let unique_eqids = {}
+    //     let unique_eqids = {}
         
-        eqidsToParse.forEach(v => {
-            unique_eqids[v[0]] ? unique_eqids[v[0]] +=1 : unique_eqids[v[0]] = 1;
-        });
+    //     eqidsToParse.forEach(v => {
+    //         unique_eqids[v[0]] ? unique_eqids[v[0]] +=1 : unique_eqids[v[0]] = 1;
+    //     });
 
-        const browser = new Browser(false);
-        await browser.init(false, 15);
-        const authPage = await browser.auth(user, pw, PIN);
-        await browser.createNotes({
-            'date': date,
-            'fName': fName,
-            'lName': lName,
-            'details': details
-        }, unique_eqids);
+    const fName = process.argv[2];
+    const lName = process.argv[3];
+    const user = process.argv[4];
+    const pw = process.argv[5];
+    const PIN = process.argv[6].split('');
+    const date = process.argv[7]
+    const eqids = process.argv[8];
+    const details = process.argv[9];
 
-        // await authPage.close();
-        // browser.disconnect();
+    let unique_eqids = {}
+    
+    eqids.forEach(v => {
+        unique_eqids[v[0]] ? unique_eqids[v[0]] +=1 : unique_eqids[v[0]] = 1;
+    });
 
-        console.log('Contact notes are ready to save!');
-        console.log('Close the browser when finished.');
-    }
+    const browser = new Browser(false);
+    await browser.init(false, 15);
+    const authPage = await browser.auth(user, pw, PIN);
+    await browser.createNotes({
+        'date': date,
+        'fName': fName,
+        'lName': lName,
+        'details': details
+    }, unique_eqids);
+
+    // await authPage.close();
+    // browser.disconnect();
+
+    console.log('Contact notes are ready to save!');
+    console.log('Close the browser when finished.');
+    
 }
 
 function getDetailFile(files) {
@@ -76,7 +95,7 @@ function getEQIDsFile(files) {
     }
 }
 
-main();
+//main();
 
 
 // get details
--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
+import subprocess
 import tkinter as tk
 import tkinter.scrolledtext as sb
+from tkinter import ttk
 fields = (
     {
         'label' : 'Staff first name',
@@ -27,6 +29,11 @@ fields = (
         'large': False
     },
     {
+        'label' : 'Date of contact (dd-mm)',
+        'protected': False,
+        'large': False
+    },
+    {
         'label' : 'Paste a list of EQIDs\n(or the footer from a compass email)',
         'protected': False,
         'large': True
@@ -37,45 +44,62 @@ fields = (
         'large': True
     }
 )
-def makeform(window, fields):
-    entries = {}
+
+def horizonal_rule(frame):
+    sep = ttk.Separator(frame, orient='horizontal')
+    sep.pack(fill=tk.X)
+
+def make_header(window, header):
+    top = ttk.Frame(window)
+    header = ttk.Label(top, text=header, anchor='n', style='Header.TLabel')
+    top.pack(side = tk.TOP, fill = tk.X, padx = 5 , pady = 25)
+    header.pack(side = tk.TOP, fill = tk.X)
+    horizonal_rule(top)
+
+def make_form(window, fields):
+    entries = []
     first = True
     for field in fields:
-        row = tk.Frame(window, bg='slate gray', bd=2)
-        lab = tk.Label(row, width=32, text=field['label'] + ": ", anchor='w', fg='white', bg='slate gray')
+        row = ttk.Frame(window)
+        lab = ttk.Label(row, width=32, text=field['label'] + ': ', anchor='w')
         if field['large']:
-            ent = sb.ScrolledText(row, height=10, fg='white', bg='gray24', width=32, insertbackground='white', relief=tk.GROOVE)
-            # scroll = tk.Scrollbar(ent)
-            # scroll.pack( side = tk.RIGHT, fill = tk.Y )
-            # scroll.config( command = ent.yview )
-            # ent['yscrollcommand'] = scroll.set
+            ent = sb.ScrolledText(row, height=10, fg='white', bg='gray24', width=32, insertbackground='white', relief=tk.GROOVE, font=('helvetica', 11))
+            entries.append((ent, lambda ent: ent.get('1.0', tk.END)))
         else:
-            ent = tk.Entry(row, fg='white', bg='gray24', 
-            show='*' if field['protected'] else '', width=32, insertbackground='white')
+            ent = tk.Entry(row, show='*' if field['protected'] else '', width=32, foreground='white', background='#3D3D3D', font=('helvetica', 11), insertbackground='white', insertwidth=1)
+            entries.append((ent, lambda ent: ent.get()))
             if first:
+                first = False
                 ent.focus_set()
         row.pack(side = tk.TOP, fill = tk.X, padx = 5 , pady = 5)
         lab.pack(side = tk.LEFT)
         ent.pack(side = tk.RIGHT, expand = True, fill = tk.X)
-        entries[field['label']] = ent
     return entries
 
 def create_notes(entries):
-    print(entries)
-    args = []
+    cmd = ['./dist/bulk-note.exe']
     for entry in entries:
-        entry.append(entry.get())
+        print()
+        cmd.append(f'"{entry[1](entry[0])}"')
+
+    subprocess.run(cmd)
 
 if __name__ == '__main__':
     window = tk.Tk()
+    ttk.Style().configure('Header.TLabel', foreground='white', background='slate gray', font=(42))
+    ttk.Style().configure('TLabel', foreground='white', background='slate gray', font=('helvetica', 11))
+    # ttk.Style().configure('TEntry', foreground='white', background='#3D3D3D', font=('helvetica', 11))
+    ttk.Style().configure('TFrame', background='slate gray')
+    ttk.Style().configure('TButton', padding=6, relief='raised', font=('helvetica', 11))
+
     window.resizable(0,0)
     window.config(background='slate gray')
     window.title('Create Bulk Contact Notes')
-    entry_font = {'font': ('consolas', 11)}
-    entries = makeform(window, fields)
-    # bg = tk.Frame(master=window, width=200, height=100, bg="gray")
+    make_header(window, 'Fill out the form below:')
+    entries = make_form(window, fields)
+    # bg = tk.Frame(master=window, width=200, height=100, bg='gray')
     # bg.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
     # window.bind('<Return>', (lambda: create_notes(entries)))
-    b1 = tk.Button(window, text = 'Create Contact Notes!', command = (lambda: create_notes(entries)))
-    b1.pack(side = tk.BOTTOM, padx = 5, pady = 5)
+    b1 = ttk.Button(window, text = 'Create Contact Notes!', command = (lambda: create_notes(entries)))
+    b1.pack(side = tk.BOTTOM, padx = 5, pady = 15)
     window.mainloop()
